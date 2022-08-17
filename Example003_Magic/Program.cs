43b28cde50ccb109f47b4ebcefdefe5460bcab07
marinamyazina <marinasochi1@rambler.ru>
2022-08-18 00:50:50 +0300

    Построили три вершины

---
+++ Example003_Magic/Program.cs
@@ -0,0 +1,24 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Console.SetCursorPosition (10,4);
+        // Console.WriteLine ("+");
+        Console.Clear();
+
+        int xa = 1, ya = 1,
+            xb = 6, yb = 5,
+            xc = 80, yc = 10;
+
+        Console.SetCursorPosition(xa,ya);
+        Console.WriteLine("+");
+
+        Console.SetCursorPosition(xb,yb);
+        Console.WriteLine("+");
+
+        Console.SetCursorPosition(xc,yc);
+        Console.WriteLine("+");
+       
+    }
+}
