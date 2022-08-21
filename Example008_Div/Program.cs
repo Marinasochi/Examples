5d6e0b590aaa59f58d77ac1261edbc90c05021e9
marinamyazina <marinasochi1@rambler.ru>
2022-08-21 21:17:03 +0300

    Выполнили Задание 8

---
+++ Example008_Div/Program.cs
@@ -0,0 +1,14 @@
+﻿// Деление
+double Prompt(string message)
+{
+    System.Console.Write(message);    // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    double result = double.Parse(readValue);    // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+double numberA = Prompt("Введите numberA  ");
+double numberB = Prompt("Введите numberB  ");
+{
+    Console.Write($"numberA / numberB = {numberA / numberB}");
+}
