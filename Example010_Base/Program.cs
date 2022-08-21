d806e8e7f61661390baa0cef42cb52baa2fdaaa3
marinamyazina <marinasochi1@rambler.ru>
2022-08-22 01:28:51 +0300

    Выполнили Задание 10

---
+++ Example010_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск максимального элемента
+int a = 5;
+int b = 85;
+int c = 5;
+int d = -8;
+int e = -521;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine($"max = {max}");
