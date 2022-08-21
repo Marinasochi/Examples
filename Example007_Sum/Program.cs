2f3f1f8e6f9e89eddab82695afb991638dee331e
marinamyazina <marinasochi1@rambler.ru>
2022-08-21 20:47:47 +0300

    Выполнили Задание 7

---
+++ Example007_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Сумма двух чисел
+int Prompt(string message)
+{
+     Console.Write(message);           // Вывести сообщение
+     string readValue = Console.ReadLine();   // Считывает с консоли строку
+     int result = int.Parse(readValue);    // Преобразует строку в целое число
+     return result;                           // Возвращает результат функции  }
+}
+
+int numberA = Prompt("Введите число A ");
+int numberB = Prompt("Введите число B ");
+
+{
+    Console.WriteLine(numberA + numberB);
+}
