4432a34e07065e9e3315eff0436b9b7e2e2e4c62
marinamyazina <marinasochi1@rambler.ru>
2022-09-14 21:12:43 +0300

    add "Выполнили Example013"

---
+++ Example013_MassFind/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {78,12,6,12,0,56,47,12,12};
+int n = array.Length;
+int index=0;
+int find = 12;
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
